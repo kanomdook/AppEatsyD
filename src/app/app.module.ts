5a63d0a2bbf21d3e456dd4cba0aaa3b810c2fad3
kanomdook <kanung.2535@gmail.com>
2017-10-25 11:01:12 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,71 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+//*********** ionic Native **************/
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Geolocation } from '@ionic-native/geolocation';
+
+import { MyApp } from './app.component';
+
+//***********  Angularfire2 v4 **************/
+
+import { AngularFireModule } from 'angularfire2';
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+//*********** Provider **************/
+import { AuthData } from '../providers/auth-data';
+import { RadioPlayer } from '../providers/radio-service';
+import { Firebase } from '@ionic-native/firebase';
+
+//*********** Image Gallery **************/
+import { GalleryModal } from 'ionic-gallery-modal';
+import { ZoomableImage } from 'ionic-gallery-modal';
+
+
+//********** firebase configuration  ************ */
+export const config = {
+  apiKey: "AIzaSyAKkCULXo1SNuBRN4KRzHl9D2DxV8LzZWE",
+  authDomain: "testdev-475e6.firebaseapp.com",
+  databaseURL: "https://testdev-475e6.firebaseio.com",
+  projectId: "testdev-475e6",
+  storageBucket: "testdev-475e6.appspot.com",
+  messagingSenderId: "307498376583"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    GalleryModal,
+    ZoomableImage,
+
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    GalleryModal,
+    ZoomableImage,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Geolocation,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    AuthData,
+    RadioPlayer,
+    Firebase
+  ]
+})
+export class AppModule { }
